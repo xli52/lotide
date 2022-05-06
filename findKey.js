9324d71ab5bb444c3350afb20c71c0b8d36db43c
Xiang Li <lxmichael52@gmail.com>
2022-05-05 22:06:26 -0700

    findKey.js added

---
+++ findKey.js
@@ -0,0 +1,22 @@
+const findKey = function(object, callback) {
+  for (key in object) {
+    if (callback(object[key])) return key;
+  }
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Asssertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
