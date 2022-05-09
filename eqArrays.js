3c35da1c1efb776b03469b689c14e39851be9a84
Xiang Li <lxmichael52@gmail.com>
2022-05-09 16:40:57 -0700

    eqArrays.js refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Asssertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function (firstArray, secondArray) {
   
@@ -24,4 +18,4 @@ const eqArrays = function (firstArray, secondArray) {
 }
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
